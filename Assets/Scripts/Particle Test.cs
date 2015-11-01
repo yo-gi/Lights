bbc42e0ab731168aa0fde9e453545b3365dcdafc
amesjm <amesjm@umich.edu>
2015-11-01 15:16:37 -0500

    Added the Power of Fire

---
+++ Assets/Scripts/Particle Test.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ParticleTest : MonoBehaviour {
+	void Update () {
+		GetComponent<ParticleSystem>().startSize = transform.lossyScale.magnitude;
+	}
+}
