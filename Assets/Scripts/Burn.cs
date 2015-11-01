3c0f8e0ebfdc2dcb7cbf07e329357e4656a0a258
amesjm <amesjm@umich.edu>
2015-11-01 17:55:12 -0500

    Added Player Burning

---
+++ Assets/Scripts/Burn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Burn : MonoBehaviour {
+
+	public static Burn S;
+
+	public float burnRate;
+
+	public bool ________________;
+
+	public float timeRemaining = 0;
+	public float nextTick;
+
+	void Awake()
+	{
+		S = this;
+	}
+
+	public void setBurning(float burntime)
+	{
+		timeRemaining = Time.time + burntime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeRemaining > Time.time && Time.time > nextTick) {
+			//change limin
+			print("fire tick!");
+			nextTick = Time.time + burnRate;
+		}
+	}
+}
--- Assets/Scripts/Particle Test.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-[ExecuteInEditMode]
-public class ParticleTest : MonoBehaviour {
-	void Update () {
-		GetComponent<ParticleSystem>().startSize = transform.lossyScale.magnitude;
-	}
-}
