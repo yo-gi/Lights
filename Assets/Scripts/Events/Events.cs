3283e4e5089318215cefe3ca1556701f19fc7150
Loic Sharma <sharma.loic@gmail.com>
2015-11-06 19:08:38 -0500

    Fixed issue to events class.

---
+++ Assets/Scripts/Events/Events.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Event {
+	OnReset,
+	TorchesLit,
+	AltarsActivated,
+	LevelComplete
+}
+
+public class Events {
+	
+	private static Dictionary<Event, List<Action>> handlers = new Dictionary<Event, List<Action>>();
+	
+	public static void Register(Event e, Action callback) {
+		if (Events.handlers.ContainsKey(e) == false) {
+			Events.handlers[e] = new List<Action>();
+		}
+		
+		Events.handlers[e].Add(callback);
+	}
+	
+	public static void Broadcast(Event e) {
+		if (Events.handlers.ContainsKey(e) == false) return;
+		
+		foreach (var events in Events.handlers[e]) {
+			events();
+		}
+	}
+}
