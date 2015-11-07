b7d157d5ff7a2434e302fa38d0fb07225f39b9f2
Loic Sharma <sharma.loic@gmail.com>
2015-11-06 20:33:22 -0500

    Added trail to teleport script.

--- Assets/Scripts/Movement/Teleport.cs
+++ Assets/Scripts/Movement/Teleport.cs
@@ -9,14 +9,21 @@ public class Teleport : MonoBehaviour {
 	private float lastTeleport = 0;
 	private Queue<Location> locationHistory = new Queue<Location>();
 
+	private Object trailPrefab;
+	private Object trail;
+
 	public class Location {
 		public Vector3 vector;
 		public float time;
 	}
 
 	void Awake() {
+		this.trailPrefab = Resources.Load("Teleport Trail");
+
 		Events.Register<OnResetEvent>(this.ResetLocationHistory);
 		Events.Register<OnLevelCompleteEvent>(this.ResetLocationHistory);
+
+		this.CreateTrailObject();
 	}
 
 	void Update() {
@@ -28,11 +35,21 @@ public class Teleport : MonoBehaviour {
 
 		this.UpdateLocationHistory(this.gameObject.transform.position);
 	}
-
+	
 	private void ResetLocationHistory() {
 		this.lastTeleport = Time.time;
-		
+
+		Destroy(this.trail);
+
 		this.locationHistory.Clear();
+		this.CreateTrailObject();
+	}
+
+	private void CreateTrailObject() {
+		GameObject trail = (GameObject) Instantiate(this.trailPrefab, this.transform.position, this.transform.rotation);
+
+		trail.transform.parent = this.transform;
+		this.trail = trail;
 	}
 
 	private bool TeleportIsAvailable() {
