bd57578a131fffcfb3a86ccdf4b0eec46e4c5bfd
Yogesh Seetharaman <yogeshs@umich.edu>
2015-11-08 22:56:03 -0500

    anim

--- Assets/Scripts/Navi.cs
+++ Assets/Scripts/Navi.cs
@@ -8,8 +8,10 @@ public class Navi : MonoBehaviour
 	
 	public float lerpTime;
 	public float lerpSpeed;
-	public float xOffset;
-	public float yOffset;
+	public float randXOffset;
+	public float randYOffset;
+	public float followX;
+	public float followY;
 
 	public float maxLightRadius;
 	public float recoveryRate;
@@ -35,7 +37,7 @@ public class Navi : MonoBehaviour
 		naviLight = GameObject.Find("Navi Light").GetComponent<LOSRadialLight>();
         sprite.color = naviLight.color;
 		end = Player.S.gameObject.transform.position;
-		InvokeRepeating ("orbit", 0, lerpTime + 0.1f);
+		InvokeRepeating ("orbit", 0, lerpTime);
 	}
 
 	void orbit ()
@@ -43,17 +45,14 @@ public class Navi : MonoBehaviour
 
 		startTime = Time.time;
 		Vector3 p = Player.S.gameObject.transform.position;
-		end = new Vector3 (p.x + Random.Range (-1 * xOffset, xOffset), p.y + Random.Range (1, yOffset));
+		end = new Vector3 (p.x + followX + Random.Range (-randXOffset, randXOffset), p.y + followY + Random.Range (-randYOffset, randYOffset));
 		length = Vector3.Distance (transform.position, end);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		/*if (Luminosity <= 0) {
-			Luminosity = 100;
-			Door.switchLevels(MainCam.level == 1 ? 5 : MainCam.level - 1);
-		}*/
+
 		float distanceCovered = (Time.time - startTime) * lerpSpeed;
 		float fracCovered = distanceCovered / length;
 		transform.position = Vector3.Lerp (transform.position, end, fracCovered);
