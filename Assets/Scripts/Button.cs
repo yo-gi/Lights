7238574dd56f9ffc09aa8e4327ce858033ef89c3
Loic Sharma <sharma.loic@gmail.com>
2015-11-17 17:19:34 -0500

    Added button, can be used to listen to button events.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public int id;
+
+	void Awake() {
+		this.enabled = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag != "Player") return;
+
+		this.enabled = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag != "Player") return;
+
+		this.enabled = false;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Events.Broadcast(new ButtonEvent {
+				id = id,
+				level = 0, // TODO: Use actual level.
+			});
+		}
+	}
+}
