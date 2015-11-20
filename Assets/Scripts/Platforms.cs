a9eddf23af9217c661cf1d9ad689e20989c27251
Yogesh Seetharaman <yogeshs@umich.edu>
2015-11-20 13:26:34 -0500

    Added pause ability to platforms

--- Assets/Scripts/Platforms.cs
+++ Assets/Scripts/Platforms.cs
@@ -6,6 +6,7 @@ public class Platforms : MonoBehaviour {
 	public Vector3 start;
 	public Vector3 end;
 	public float lerpSpeed;
+	public float platformPauseTime;
 	public bool active = true;
 
 	float startTime;
@@ -16,29 +17,38 @@ public class Platforms : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find("Player");
+		StartCoroutine("MovePlatform");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (!active)
-			return;
 
-		if (transform.position == end) {
-			startTime = Time.time;
-			Vector3 temp = new Vector3(end.x, end.y, end.z);
-			end = start;
-			start = temp;
+	}
+
+	IEnumerator MovePlatform() {
+		while (active) {
+			if (transform.position == end) {
+				startTime = Time.time;
+				Vector3 temp = new Vector3(end.x, end.y, end.z);
+				end = start;
+				start = temp;
+				yield return new WaitForSeconds(platformPauseTime);
+			}
+			
+			float length = Vector3.Distance (transform.position, end);
+			float distanceCovered = (Time.time - startTime) * lerpSpeed;
+			float fracCovered = distanceCovered / length;
+			Vector3 newPos = Vector3.Lerp (transform.position, end, fracCovered);
+			Vector3 diff = newPos - transform.position;
+			transform.position = newPos;
+			
+			if (colliding) {
+				player.transform.position = player.transform.position + diff;
+			}
+			
+			yield return null;
 		}
-		
-		float length = Vector3.Distance (transform.position, end);
-		float distanceCovered = (Time.time - startTime) * lerpSpeed;
-		float fracCovered = distanceCovered / length;
-		Vector3 newPos = Vector3.Lerp (transform.position, end, fracCovered);
-		Vector3 diff = newPos - transform.position;
-		transform.position = newPos;
-		if (colliding) {
-			player.transform.position = player.transform.position + diff;
-		}
+
 	}
 
 	void OnCollisionEnter2D(Collision2D c) {
