eaedc03a67eb9514e4b45f432ef723b910f837ee
Loic Sharma <sharma.loic@gmail.com>
2015-11-22 13:37:29 -0500

    Fixed button spacing.

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,30 +4,30 @@ public class Button : MonoBehaviour {
 
     public static readonly KeyCode ButtonKey = KeyCode.Space;
 
-	public int id;
+    public int id;
 
-	void Awake() {
-		enabled = false;
-	}
+    void Awake() {
+        enabled = false;
+    }
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag != "Player") return;
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag != "Player") return;
 
-		enabled = true;
-	}
+        enabled = true;
+    }
 
-	void OnTriggerExit2D(Collider2D other) {
-		if (other.tag != "Player") return;
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag != "Player") return;
 
-		enabled = false;
-	}
+        enabled = false;
+    }
 
-	void Update() {
-		if (Input.GetKeyDown(ButtonKey)) {
+    void Update() {
+        if (Input.GetKeyDown(ButtonKey)) {
             Events.Broadcast(new ButtonEvent {
                 id = id,
                 level = MainCam.currentLevel
-			});
-		}
-	}
+            });
+        }
+    }
 }
