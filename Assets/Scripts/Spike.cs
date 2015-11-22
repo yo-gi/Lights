8e85e244d15aaf018947fc8e2b7529bed2c7a16d
Loic Sharma <sharma.loic@gmail.com>
2015-11-22 17:18:09 -0500

    Added damage on touching spikes

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spike : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject == Player.S.gameObject) {
+            Events.Broadcast(new OnDeathEvent());
+        }
+    }
+}
