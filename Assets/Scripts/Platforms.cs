fd85f78deee7de0534d0bf6ee1c4735bb91f1cec
Loic Sharma <sharma.loic@gmail.com>
2015-11-22 21:37:04 -0500

    Rewrote platforms to not destroy our FPS.

--- Assets/Scripts/Platforms.cs
+++ Assets/Scripts/Platforms.cs
@@ -7,48 +7,47 @@ public class Platforms : MonoBehaviour
     public Vector3 end;
     public float lerpSpeed;
     public float platformPauseTime;
-    public bool active = true;
 
     float startTime;
 
     GameObject player;
     bool colliding = false;
 
+    private float waitUntil;
+
     // Use this for initialization
     void Start()
     {
         player = GameObject.Find("Player");
-        StartCoroutine("MovePlatform");
     }
 
-    IEnumerator MovePlatform()
+    void Update()
     {
-        while (active)
+        var currentTime = Time.time;
+
+        if (currentTime < waitUntil) return;
+
+        if (transform.position == end)
         {
-            if (transform.position == end)
-            {
-                startTime = Time.time;
-                Vector3 temp = new Vector3(end.x, end.y, end.z);
-                end = start;
-                start = temp;
-                yield return new WaitForSeconds(platformPauseTime);
-            }
+            startTime = currentTime;
+            Vector3 temp = new Vector3(end.x, end.y, end.z);
+            end = start;
+            start = temp;
 
-            float length = Vector3.Distance(transform.position, end);
-            float distanceCovered = (Time.time - startTime) * lerpSpeed;
-            float fracCovered = distanceCovered / length;
-            Vector3 newPos = Vector3.Lerp(transform.position, end, fracCovered);
-            Vector3 diff = newPos - transform.position;
-            transform.position = newPos;
-
-            if (colliding)
-            {
-                player.transform.position = player.transform.position + diff;
-            }
-
-            yield return null;
+            waitUntil = platformPauseTime + currentTime;
         }
 
+        float length = Vector3.Distance(transform.position, end);
+        float distanceCovered = (Time.time - startTime) * lerpSpeed;
+        float fracCovered = distanceCovered / length;
+        Vector3 newPos = Vector3.Lerp(transform.position, end, fracCovered);
+        Vector3 diff = newPos - transform.position;
+        transform.position = newPos;
+
+        if (colliding)
+        {
+            player.transform.position = player.transform.position + diff;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D c)
