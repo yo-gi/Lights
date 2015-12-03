3effd63b79d3d0fea02086f9e979a515b7147a2a
Loic Sharma <sharma.loic@gmail.com>
2015-12-03 00:58:52 -0500

    WIP - Added cutscene script + basic example

---
+++ Assets/Scripts/Cutscenes/BossCutscene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BossCutscene : Cutscene {
+
+    float start = -1f;
+
+    protected override bool PlayCustcene() {
+        if (start < 0f) {
+            start = Time.time + 5f;
+        }
+
+        if (Time.time > start) {
+            return false;
+        }
+        else {
+            Debug.Log("Hi");
+            
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/Cutscenes/Cutscene.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class OnCutsceneStartEvent {
+    public int id;
+}
+
+public class OnCutsceneEndEvent {
+    public int id;
+}
+
+public abstract class Cutscene : MonoBehaviour {
+
+    public int id;
+
+    void Awake() {
+        if (this.GetComponent<CircleCollider2D>() == null
+            && this.GetComponent<BoxCollider2D>() == null) {
+            Debug.Log("Cutscene " + this.name + " does not have a collider!");
+        }
+
+        this.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject != Player.S.gameObject) return;
+
+        this.enabled = true;
+
+        Events.Broadcast(new OnPauseEvent { paused = true });
+        Events.Broadcast(new OnCutsceneStartEvent { id = this.id });
+    }
+
+    void Update() {
+        if (this.PlayCustcene() == false) {
+            this.enabled = false;
+
+            Events.Broadcast(new OnCutsceneEndEvent { id = this.id });
+            Events.Broadcast(new OnPauseEvent { paused = false });
+        }
+    }
+
+    protected abstract bool PlayCustcene();
+}
