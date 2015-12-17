eb303fd4cbbcfb78cbcba6175b89a01a01fe4a1c
Reece LaFortune <ReeceLaFortune@gmail.com>
2015-12-16 22:17:23 -0500

    Improved cutscenes and added dialog zones

--- Assets/Scripts/BossMusic.cs
+++ Assets/Scripts/BossMusic.cs
@@ -4,6 +4,7 @@ public class BossMusic : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other) {
         Music.S.setBossMusic();
+        MainCam.S.gameObject.GetComponent<Camera>().orthographicSize = 10;
 
         Destroy(this.gameObject);
     }
